8f8b0ae7c87319d8b329500dfeb7a39d70349539
Archibalt <132143853+Pan-Artur@users.noreply.github.com>
2025-03-16 15:16:34 +0200

    Revert "addition layout for categories"

--- src/pages/Main/Categories/List/Item/Button.jsx
+++ src/pages/Main/Categories/List/Item/Button.jsx
@@ -1,9 +1,5 @@
-export const Button = ({href = "#", className="categories__button", iconHref="./images/symbol-defs.svg#icon-upper-arrow"}) => {
-  return (
-    <a type="button" className={className} href={href}>
-      <svg class="categories__icon">
-        <use href={iconHref}></use>
-      </svg>
-    </a>
-  );
+export const Button = () => {
+    return <>
+
+    </>;
 };
--- src/pages/Main/Categories/Title.jsx
+++
@@ -1,7 +0,0 @@
-export const Title = () => {
-    return (
-        <h2 class="categories__title">
-            Популярні категорії
-        </h2>
-    );
-};

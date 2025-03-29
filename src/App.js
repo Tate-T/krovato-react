d15bcd5574067c7934a35f3837c6e5694ee4e627
Maksym Ryaboshapka <e.ryaboshapka777@gmail.com>
2025-03-29 19:22:56 +0200

    payment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import { PaymentPage } from "./pages/PaymentPage/PaymentPage";
 
 function App() {
   return (
     <div className="App">
-      
+      <PaymentPage />
     </div>
   );
 }

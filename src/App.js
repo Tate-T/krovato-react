f41e3b05c607aef693d8399a2b059ddcefa813f8
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2025-03-30 14:11:04 +0300

    conditions layout and scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ProductCardPage from './pages/ProductCard/ProductCardPage';
 
 function App() {
   return (
     <div className="App">
-      
+      <ProductCardPage/>
     </div>
   );
 }

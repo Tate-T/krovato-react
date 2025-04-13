ef4ca898e50fbd6b167ffcbd4ac1a4495dc255f0
Maksym Ryaboshapka <e.ryaboshapka777@gmail.com>
2025-04-13 09:06:21 +0300

    add subcategories component with styles and pictures

---
+++ src/pages/Catalog/Catalog.jsx
@@ -0,0 +1,13 @@
+import { Subcategories } from "./Subcategoties/Subcategories";
+
+export const Catalog = () => {
+  return (
+    <>
+      <header></header>
+      <main>
+        <Subcategories />
+      </main>
+      <footer></footer>
+    </>
+  )
+};

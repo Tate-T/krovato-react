24caae5eeda3ae587bb3f7fd4ee4507830382f30
ByteMe6 <tsapiy.yurka@gmail.com>
2025-05-11 13:55:31 +0300

    just a header

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,11 @@
-// import { Header } from "../../components/Header/Header";
+import Header from "../../components/Header/Header";
 import { Hero } from "./Hero/Hero";
 import { Categories } from "./Categories/Categories";
 import { Products } from "./Products/Products";
 import { Promotions } from "./Promotions/Promotions";
 import { LastReviews } from "../../components/LastReviews/LastReviews";
 import { Articles } from "./Articles/Articles";
-import { About } from "../../components/About/About";
+import About from "../../components/About/About";
 import { Advantages } from "../../components/Advantages/Advantages";
 import { Footer } from "../../components/Footer/Footer";
 
@@ -13,6 +13,7 @@ export const Main = () => {
   return (
     <>
       {/* Header */}
+      <Header/>
       <main>
         <Hero />
         <Categories />
@@ -20,7 +21,7 @@ export const Main = () => {
         <Promotions />
         <LastReviews />
         <Articles />
-        <About />
+        {/* <About /> */}
         <Advantages />
       </main>
       <Footer />

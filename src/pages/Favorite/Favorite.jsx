ad62ed3bc373e2e98b5c2822fa449b80e1357a7a
Serhii-Zamorylo23 <129538617+Serhii-Zamorylo23@users.noreply.github.com>
2025-05-11 23:25:44 +0300

    start

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -1,11 +1,9 @@
-import { Footer } from "../../components/Footer/Footer"
-import Header from "../../components/Header/Header";
-
 import style from './Favorite.module.scss'
 
 export const FavoritePage = () => {
-  return (<div className="FavoritePage">
-    <Header/>
-    <Footer/>
-  </div>)
+  return (
+  <div className="FavoritePage">
+
+  </div>
+  )
 }

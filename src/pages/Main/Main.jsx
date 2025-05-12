fc73b58a00b695e5862ae5fa00552ac785687f64
Nikita <nik200830@gmail.com>
2025-05-12 10:30:30 +0300

    about-layout

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,7 +5,7 @@ import { Products } from "./Products/Products";
 import { Promotions } from "./Promotions/Promotions";
 import { LastReviews } from "../../components/LastReviews/LastReviews";
 import { Articles } from "./Articles/Articles";
-import About from "../../components/About/About";
+import { About } from "../../components/About/About";
 import { Advantages } from "../../components/Advantages/Advantages";
 import { Footer } from "../../components/Footer/Footer";
 export const Main = () => {
@@ -20,7 +20,7 @@ export const Main = () => {
         <Promotions />
         <LastReviews />
         <Articles />
-        {/* <About /> */}
+        <About />
         <Advantages />
       </main>
       <Footer />

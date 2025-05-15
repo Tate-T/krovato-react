e7f45646d68d32a85496ce15fffaf5deae479664
Nesterenko <93931811+Danya3213@users.noreply.github.com>
2025-05-15 20:04:41 +0300

    problem fixed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 
- import { Main } from './pages/Main/Main';
-// import { PaymentPage } from './pages/PaymentPage/PaymentPage';
-// import { Delivery } from './pages/Delivery/Delivery';
-// import { ContactsPage } from './pages/ContactsPage/ContactsPage';
-// import { LastReviews } from './components/LastReviews/LastReviews';
-// import { ProductCardPage } from './pages/ProductCard/ProductCardPage';
+import { Main } from './pages/Main/Main';
+import { PaymentPage } from './pages/PaymentPage/PaymentPage';
+import { Delivery } from './pages/Delivery/Delivery';
+import { ContactsPage } from './pages/ContactsPage/ContactsPage';
+import { LastReviews } from './components/LastReviews/LastReviews';
+import { ProductCardPage } from './pages/ProductCard/ProductCardPage';
 
 function App() {
   return (
     <div className="App">
-     <Main /> 
-      {/* <PaymentPage /> */}
-      {/* <Delivery /> */}
-      {/* <ContactsPage /> */}
-      {/* <LastReviews /> */}
-      {/* <ProductCardPage /> */}
+      <Main /> 
+      <PaymentPage />
+      <Delivery />
+      <ContactsPage />
+      <LastReviews />
+      <ProductCardPage />
     </div>
   );
 }

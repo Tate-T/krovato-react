cfb8b376778e65fe94a16a34e95b20fc2fe93dcc
Nesterenko <93931811+Danya3213@users.noreply.github.com>
2025-05-17 19:34:09 +0300

    problem fix

--- src/pages/Main/Products/List/List.jsx
+++ src/pages/Main/Products/List/List.jsx
@@ -4,28 +4,25 @@ import style from "../Products.module.scss";
 
 import { useState, useEffect } from "react";
 
-import products from '../../../../products';
+import products from "../../../../products";
 
 export const ProductsList = () => {
-
   const [product, setProduct] = useState([]);
 
-  function activeProducts (productId) {
-
-    if (product.find(item => item.id === productId)) {
-     
-      setProduct(prev => prev.filter(item => item.id !== productId));
+  function activeProducts(productId) {
+    if (product.find((item) => item.id === productId)) {
+      setProduct((prev) => prev.filter((item) => item.id !== productId));
     } else {
-
-      setProduct(prev => [...prev, products.find(item => item.id === productId)]);
-    };
-  };
+      setProduct((prev) => [
+        ...prev,
+        products.find((item) => item.id === productId),
+      ]);
+    }
+  }
 
   useEffect(() => {
-
-    localStorage.setItem('activeProducts', JSON.stringify(product));
-    console.log('localStorage updated');
-    
+    localStorage.setItem("activeProducts", JSON.stringify(product));
+    console.log("localStorage updated");
   }, [product]);
 
   return (

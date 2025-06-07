aa416acf3f0e10397199db3c8b324ad7a9010dd7
ind0zer <165696434+ind0zer@users.noreply.github.com>
2025-06-07 22:26:49 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 
-// import { Main } from './pages/Main/Main';
+import { Main } from './pages/Main/Main';
 // import { PaymentPage } from './pages/PaymentPage/PaymentPage';
 // import { Delivery } from './pages/Delivery/Delivery';
-import { ContactsPage } from './pages/ContactsPage/ContactsPage';
-// import { LastReviews } from './components/LastReviews/LastReviews';
-// import { ProductCardPage } from './pages/ProductCard/ProductCardPage';
+// import { ContactsPage } from './pages/ContactsPage/ContactsPage';
+// // import { LastReviews } from './components/LastReviews/LastReviews';
+// // import { ProductCardPage } from './pages/ProductCard/ProductCardPage';
 
 function App() {
   return (
     <div className="App">
-      {/* <Main /> 
-      <PaymentPage />
+      <Main /> 
+      {/* <PaymentPage />
       <Delivery /> */}
-      <ContactsPage />
+      {/* <ContactsPage /> */}
       {/* <LastReviews />
       <ProductCardPage /> */}
     </div>

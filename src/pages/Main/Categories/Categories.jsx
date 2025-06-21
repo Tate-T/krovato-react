d11d7e53659d9c579b480ea3ea626354bcc246cc
Nesterenko <93931811+Danya3213@users.noreply.github.com>
2025-06-21 19:26:24 +0300

    hooks

--- src/pages/Main/Categories/Categories.jsx
+++ src/pages/Main/Categories/Categories.jsx
@@ -2,60 +2,37 @@ import { CategoriesList } from "./List/List";
 
 import Container from "../../../components/Container/Container";
 import style from "./Categories.module.scss";
-import { Component } from "react";
+import { useState, useEffect } from "react";
 
-// export const Categories = () => {
-//   return (
-//     <section className={style.categories}>
-//       <Container className={style.categories__container}>
-//         <h2
-//           data-aos="zoom-out-right"
-//           data-aos-duration="1500"
-//           className={style.categories__title}
-//         >
-//           Популярні категорії
-//         </h2>
-//         <CategoriesList />
-//       </Container>
-//     </section>
-//   );
-// };
+export function Categories() {
+  const [data, setData] = useState([]);
 
-export class Categories extends Component {
-  state = {
-    data: [],
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     try {
       const data = fetch(
         "https://6832fc72c3f2222a8cb48d25.mockapi.io/promo/categories"
       )
         .then((response) => response.json())
-        .then((data) => {
-          this.setState({
-            promos: data,
-          });
+        .then((dataE) => {
+          setData(dataE);
         });
     } catch (error) {
       console.log("server error" + error);
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <section className={style.categories}>
-        <Container className={style.categories__container}>
-          <h2
-            data-aos="zoom-out-right"
-            data-aos-duration="1500"
-            className={style.categories__title}
-          >
-            Популярні категорії
-          </h2>
-          <CategoriesList data={this.state.data} />
-        </Container>
-      </section>
-    );
-  }
+  return (
+    <section className={style.categories}>
+      <Container className={style.categories__container}>
+        <h2
+          data-aos="zoom-out-right"
+          data-aos-duration="1500"
+          className={style.categories__title}
+        >
+          Популярні категорії
+        </h2>
+        <CategoriesList data={data} />
+      </Container>
+    </section>
+  );
 }

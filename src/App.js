24a1c90d01a64c015942185a09b951ed05b4415e
DimaVax <126974388+DimaVax@users.noreply.github.com>
2025-06-22 12:58:51 +0200

    blablabla

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import './App.css';
 
-// import { Main } from './pages/Main/Main';
-// import { PaymentPage } from './pages/PaymentPage/PaymentPage';
+import { Main } from './pages/Main/Main';
+import { PaymentPage } from './pages/PaymentPage/PaymentPage';
 // import { Delivery } from './pages/Delivery/Delivery';
-// import { ContactsPage } from './pages/ContactsPage/ContactsPage';
-// import { LastReviews } from './components/LastReviews/LastReviews';
-// import { ProductCardPage } from './pages/ProductCard/ProductCardPage';
-// import { CartModal } from './components/BasketModal/BasketModal';
+import { ContactsPage } from './pages/ContactsPage/ContactsPage';
+import { LastReviews } from './components/LastReviews/LastReviews';
+import { ProductCardPage } from './pages/ProductCard/ProductCardPage';
+import { CartModal } from './components/BasketModal/BasketModal';
 
 import { Questions } from './components/Questions/Questions';
 
 function App() {
   return (
     <div className="App">
-      {/* <Main /> 
+      <Main /> 
       <PaymentPage />
-      <Delivery />
+      {/* <Delivery /> */}
       <ContactsPage />
       <LastReviews />
       <ProductCardPage />
-      <CartModal /> */}
+      <CartModal />
 
-      <Questions sectionId="bed-questions" />
-      <Questions sectionId="return-questions" />
+      {/* <Questions sectionId="bed-questions" />
+      <Questions sectionId="return-questions" /> */}
     </div>
   );
 }

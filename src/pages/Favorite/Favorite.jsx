1522732a73efcd62bef81b0c285664f9a4eda809
Serhii-Zamorylo23 <129538617+Serhii-Zamorylo23@users.noreply.github.com>
2025-06-22 09:38:54 +0300

    refactoring

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -5,26 +5,6 @@ import { FavoriteItem } from "./FavoriteItem";
 
 import styleFavorite from './Favorite.module.scss'
 const Favorite = () => {
-
-  // let products = JSON.parse(localStorage.getItem("activeProducts"));
-  
-  // const buttons = [
-  //   ...document.querySelectorAll("." + styleProduct.products__button),
-  // ].filter((item, index) => index % 2 !== 1);
-
-  
-  // buttons.forEach((item) => {
-
-  //   item.addEventListener("click", () => {
-  //     products = JSON.parse(localStorage.getItem("activeProducts"));
-  //   });
-  // });
-  
-  
-  // useEffect(() => {
-  //   console.log("products checked" + products);
-  // }, [products]);
-
   const [products, setProducts] = useState(() => {
 
     return JSON.parse(localStorage.getItem("activeProducts"));

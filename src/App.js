5812163f34131a49b87f6f8c7364f7f0e7befade
VoTs <126974582+v0ts@users.noreply.github.com>
2025-09-06 20:04:23 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,60 +1,61 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import { lazy,Suspense } from "react";
-// import { Header } from "./components/Header/Header";
-const Header = lazy(() => import("./components/Header/Header")) 
-// import { MainPage } from "./pages/MainPage/MainPage";
-const MainPage = lazy(() => import("./pages/MainPage/MainPage")) 
-// import { Footer } from "./components/Footer/Footer";
-const Footer = lazy(() => import("./components/Footer/Footer")) 
-// import { Catalog } from "./pages/Catalog/Catalog";
-const Catalog = lazy(() => import("./pages/Catalog/Catalog")) 
-// import {Basket} from "./pages/Basket/Basket";
-const Basket = lazy(() => import("./pages/Basket/Basket"))
-// import { AgreeTerms } from "./pages/Basket/AgreeTerms/AgreeTerms";
-const AgreeTerms = lazy(() => import("./pages/Basket/AgreeTerms/AgreeTerms"))
-// import { About } from "./components/About/About";
-const About = lazy(() => import("./components/About/About"))
-// import { PaymentPage } from "./pages/PaymentPage/PaymentPage";
-const PaymentPage = lazy(() => import("./pages/PaymentPage/PaymentPage"))
-// import Delivery from "./pages/Delivery/Delivery";
-const Delivery = lazy(() => import("./pages/Delivery/Delivery"))
-// import MainReviewsPage from "./pages/MainReviews/MainReviews";
-const MainReviewsPage = lazy(() => import("./pages/MainReviews/MainReviews"))
-// import BlogPage from "./pages/Blog/BlogPage";
-const BlogPage = lazy(() => import("./pages/Blog/BlogPage"))
-// import { BlogMain } from "./pages/Blog/BlogMain/BlogMain";
-const BlogMain = lazy(() => import("./pages/Blog/BlogMain/BlogMain"))
-// import BlogArticle from "./pages/BlogArticle/BlogArticle";
-const BlogArticle = lazy(() => import("./pages/BlogArticle/BlogArticle"))
-// import ContactsPage from "./pages/ContactsPage/ContactsPage";
-const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"))
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
+import { Header } from './components/Header/Header'
+// const Header = lazy(() => import('./components/Header/Header.jsx'))
+// // import { MainPage } from "./pages/MainPage/MainPage";
+// const MainPage = lazy(() => import('./pages/MainPage/MainPage.jsx'))
+// // import { Footer } from "./components/Footer/Footer";
+// const Footer = lazy(() => import('./components/Footer/Footer.jsx'))
+// // import { Catalog } from "./pages/Catalog/Catalog";
+// const Catalog = lazy(() => import('./pages/Catalog/Catalog.jsx'))
+// // import {Basket} from "./pages/Basket/Basket";
+// const Basket = lazy(() => import('./pages/Basket/Basket.jsx'))
+// // import { AgreeTerms } from "./pages/Basket/AgreeTerms/AgreeTerms";
+// const AgreeTerms = lazy(() => import('./pages/Basket/AgreeTerms/AgreeTerms.jsx'))
+// // import { About } from "./components/About/About";
+// const About = lazy(() => import('./components/About/About.jsx'))
+// // import { PaymentPage } from "./pages/PaymentPage/PaymentPage";
+// const PaymentPage = lazy(() => import('./pages/PaymentPage/PaymentPage.jsx'))
+// // import Delivery from "./pages/Delivery/Delivery";
+// const Delivery = lazy(() => import('./pages/Delivery/Delivery.jsx'))
+// // import MainReviewsPage from "./pages/MainReviews/MainReviews";
+// const MainReviewsPage = lazy(() => import('./pages/MainReviews/MainReviews.jsx'))
+// // import BlogPage from "./pages/Blog/BlogPage";
+// const BlogPage = lazy(() => import('./pages/Blog/BlogPage.jsx'))
+// // import { BlogMain } from "./pages/Blog/BlogMain/BlogMain";
+// const BlogMain = lazy(() => import('./pages/Blog/BlogMain/BlogMain.jsx'))
+// // import BlogArticle from "./pages/BlogArticle/BlogArticle";
+// const BlogArticle = lazy(() => import('./pages/BlogArticle/BlogArticle.jsx'))
+// // import ContactsPage from "./pages/ContactsPage/ContactsPage";
+const ContactsPage = lazy(() => import('./pages/ContactsPage/ContactsPage.jsx'))
+
+// const HeaderFooter = lazy(() => import('./pages/HeaderFooter/HeaderFooter.jsx'))
+
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <main>
-        <Suspense>
-        <Routes>
-          <Route path="/" element={<MainPage />}/>
-          <Route path="/catalog" element={<Catalog/>}/>
-          <Route path="/basket" element={<Basket/>}/>
-          <Route path="/agree" element={<AgreeTerms />}/>
-          <Route path="/aboutUs" element={<About/>}/>
-          <Route path="/pay" element={<PaymentPage/>}/>
-          <Route path="/delivery" element={<Delivery/>}/>
-          <Route path="/reviews" element={<MainReviewsPage/>}/>
-          <Route path="/blog" element={<BlogPage/>} />
-          <Route path="/blogMain" element={<BlogMain/>}/>
-          <Route path="/blogArticle" element={<BlogArticle/>}/>
-          <Route path="/contacts" element={<ContactsPage/>}/>
-          <Route path="*" element={<MainPage/>}/>
-        </Routes>
-        </Suspense>
-      </main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Suspense>
+				<Routes>
+					{/* <Route path='/' element={<HeaderFooter />}>
+							<Route index element={<MainPage />} />
+							<Route path='catalog' element={<Catalog />} />
+							<Route path='/basket' element={<Basket />} />
+							<Route path='/agree' element={<AgreeTerms />} />
+							<Route path='/aboutUs' element={<About />} />
+							<Route path='/pay' element={<PaymentPage />} />
+							<Route path='/delivery' element={<Delivery />} />
+							<Route path='/reviews' element={<MainReviewsPage />} />
+							<Route path='/blog' element={<BlogPage />} />
+							<Route path='/blogMain' element={<BlogMain />} />
+							<Route path='/blogArticle' element={<BlogArticle />} />
+							<Route path='/contacts' element={<ContactsPage />} />
+							<Route path='*' element={<MainPage />} />
+						</Route> */}
+				</Routes>
+			</Suspense>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/pages/Basket/BasketList.jsx
+++ src/pages/Basket/BasketList.jsx
@@ -1,278 +1,267 @@
-import { Fragment } from "react";
-import promoIcon from "../../images/basket/promo.svg";
-import checkIcon from "../../images/basket/check.svg";
-import arrowShow from "../../images/basket/arrow-show.svg";
-import deleteSvg from "../../images/basket/delete.svg";
+import { Fragment } from 'react'
+import promoIcon from '../../images/basket/promo.svg'
+import checkIcon from '../../images/basket/check.svg'
+import arrowShow from '../../images/basket/arrow-show.svg'
+import deleteSvg from '../../images/basket/delete.svg'
 import { useState, useEffect, useContext } from 'react'
-import styles from "./BasketList.module.scss";
-import { ContextBasketList } from "./ContextBasketList";
-import { Link } from "react-router-dom";
+import styles from './BasketList.module.scss'
+import { ContextBasketList } from './ContextBasketList'
+import { Link } from 'react-router-dom'
 const validPromoCodes = {
-  DISCOUNT10: 10,
-  SPRING20: 20,
-  FREESHIP: 0,
-};
+	DISCOUNT10: 10,
+	SPRING20: 20,
+	FREESHIP: 0,
+}
 export const BasketList = () => {
-  const [showAll, setShowAll] = useState(false)
-  const [discountPercent, setDiscountPercent] = useState(0)
-  const [promoMessage, setPromoMessage] = useState("")
-  const [promoInputValue, setPromoInputValue] = useState("");
+	const [showAll, setShowAll] = useState(false)
+	const [discountPercent, setDiscountPercent] = useState(0)
+	const [promoMessage, setPromoMessage] = useState('')
+	const [promoInputValue, setPromoInputValue] = useState('')
 
-  const [counts, setCounts] = useState([]);
+	const [counts, setCounts] = useState([])
 
+	const [items, setItems] = useState(() => {
+		const saved = localStorage.getItem('activeProducts')
+		return saved ? JSON.parse(saved) : []
+	})
+	useEffect(() => {
+		const savedCounts = localStorage.getItem('basketCounts')
+		if (savedCounts) {
+			setCounts(JSON.parse(savedCounts))
+		} else {
+			setCounts(items.map(() => 1))
+		}
+	}, [items])
 
-  const [items, setItems] = useState(() => {
-    const saved = localStorage.getItem("activeProducts");
-    return saved ? JSON.parse(saved) : [];
-  });
-  useEffect(() => {
-    const savedCounts = localStorage.getItem("basketCounts");
-    if (savedCounts) {
-      setCounts(JSON.parse(savedCounts));
-    } else {
-      setCounts(items.map(() => 1));
-    }
-  }, [items]); 
+	useEffect(() => {
+		localStorage.setItem('basketCounts', JSON.stringify(counts))
+	}, [counts])
 
+	useEffect(() => {
+		setCounts(items.map(() => 1))
+	}, [items])
+	useEffect(() => {
+		localStorage.setItem('activeProducts', JSON.stringify(items))
+	}, [items])
 
-  useEffect(() => {
-    localStorage.setItem("basketCounts", JSON.stringify(counts));
-  }, [counts]);
+	useEffect(() => {
+		const savedDiscount = localStorage.getItem('discountPercent')
+		if (savedDiscount) {
+			setDiscountPercent(JSON.parse(savedDiscount))
+		}
+	}, [])
 
-useEffect(() => {
-  setCounts(items.map(() => 1));
-}, [items]);
-  useEffect(() => {
-    localStorage.setItem("activeProducts", JSON.stringify(items));
-  }, [items]);
+	useEffect(() => {
+		localStorage.setItem('discountPercent', JSON.stringify(discountPercent))
+	}, [discountPercent])
+	const handleAddItem = (index) => {
+		const updated = [...counts]
+		updated[index]++
+		setCounts(updated)
+	}
+	const handleMinusItem = (index) => {
+		const updated = [...counts]
+		if (updated[index] > 1) {
+			updated[index]--
+			setCounts(updated)
+		}
+	}
+	const handleDeleteItem = (index) => {
+		const updatedItems = [...items]
+		const updatedCounts = [...counts]
+		updatedItems.splice(index, 1)
+		updatedCounts.splice(index, 1)
+		setItems(updatedItems)
+		setCounts(updatedCounts)
+		localStorage.setItem('activeProducts', JSON.stringify(updatedItems))
+	}
+	const toggleShowAll = () => {
+		setShowAll((prev) => !prev)
+	}
+	const applyPromo = () => {
+		if (items.length === 0) {
+			setDiscountPercent(0)
+			setPromoMessage('Товарів немає')
+			return
+		}
+		const input = promoInputValue.trim().toUpperCase()
+		if (validPromoCodes.hasOwnProperty(input)) {
+			const discount = validPromoCodes[input]
+			setDiscountPercent(discount)
+			setPromoMessage(
+				discount > 0
+					? `Промокод застосовано! Знижка ${discount}%`
+					: `Промокод застосовано! Безкоштовна доставка`
+			)
+		} else if (!input.trim('')) {
+			setPromoMessage('Порожнє поле промокоду')
+		} else {
+			setDiscountPercent(0)
+			setPromoMessage('Недійсний промокод')
+		}
+	}
 
+	const getTotalPrice = () => {
+		const total = items.reduce((acc, item, index) => {
+			const price = Number(item.price?.replace(/\D/g, '')) || 0
+			return acc + price * (counts[index] || 1)
+		}, 0)
 
-  useEffect(() => {
-    const savedDiscount = localStorage.getItem("discountPercent");
-    if (savedDiscount) {
-      setDiscountPercent(JSON.parse(savedDiscount));
-    }
-  }, []);
+		if (discountPercent > 0) {
+			return total * (1 - discountPercent / 100)
+		}
 
-
-  useEffect(() => {
-    localStorage.setItem("discountPercent", JSON.stringify(discountPercent));
-  }, [discountPercent]);
-  const handleAddItem = (index) => {
-    const updated = [...counts]
-    updated[index]++
-    setCounts(updated)
-  }
-  const handleMinusItem = (index) => {
-    const updated = [...counts]
-    if (updated[index] > 1) {
-      updated[index]--
-      setCounts(updated)
-    }
-  }
-  const handleDeleteItem = (index) => {
-    const updatedItems = [...items]
-    const updatedCounts = [...counts]
-    updatedItems.splice(index, 1)
-    updatedCounts.splice(index, 1)
-    setItems(updatedItems);
-    setCounts(updatedCounts)
-    localStorage.setItem("activeProducts", JSON.stringify(updatedItems));
-  }
-  const toggleShowAll = () => {
-    setShowAll((prev) => !prev)
-  }
-  const applyPromo = () => {
-    if(items.length === 0){
-      setDiscountPercent(0)
-      setPromoMessage("Товарів немає")
-      return;
-    }
-    const input = promoInputValue.trim().toUpperCase();
-    if (validPromoCodes.hasOwnProperty(input)) {
-      const discount = validPromoCodes[input];
-      setDiscountPercent(discount)
-      setPromoMessage(
-        discount > 0
-          ? `Промокод застосовано! Знижка ${discount}%`
-          : `Промокод застосовано! Безкоштовна доставка`
-      );
-    } else if(!input.trim("")){
-      setPromoMessage("Порожнє поле промокоду");
-    }
-    else {
-      setDiscountPercent(0);
-      setPromoMessage("Недійсний промокод");
-    }
-  }
-
-
-  const getTotalPrice = () => {
-    const total = items.reduce((acc, item, index) => {
-      const price = Number(item.price?.replace(/\D/g, "")) || 0;
-      return acc + price * (counts[index] || 1);
-    }, 0);
-
-    if (discountPercent > 0) {
-      return total * (1 - discountPercent / 100);
-    }
-
-    return total;
-  };
-  const { orderButton } = useContext(ContextBasketList)
-  return (
-    <Fragment>
-      <section>
-        <div className={styles.wrapperBasket}>
-          <p className={styles.textBasket}>
-            Ваш кошик
-            <span className={styles.textBasketNumber}>
-            {counts.reduce((acc, count) => acc + count, 0)}
-            </span>
-          </p>
-          <ul>
-            {items.map((bed, index) => {
-              const isHidden = !showAll && index >= 2;
-              return (
-                <>
-                <li
-                  key={index}
-                  className={`${styles.containerBed} ${isHidden ? styles.hiddenOnMobile : ""
-                    }`}
-                >
-                  <div className={styles.containerDescription}>
-                    <img src={bed.image.src} alt={bed.alt} className={styles.imgBed} />
-                    <div className={styles.containerCount}>
-                      <button
-                        onClick={() => handleMinusItem(index)}
-                        className={styles.btnMath}
-                      >
-                        -
-                      </button>
-                      <p>{counts[index]}</p>
-                      <button
-                        onClick={() => handleAddItem(index)}
-                        className={styles.btnMath}
-                      >
-                        +
-                      </button>
-                    </div>
-                  </div>
-                  <div className={styles.containerDescription}>
-                    <p className={styles.textBed}>{bed.size}</p>
-                    <p className={styles.textDescription}>{bed.description}</p>
-                    <div className={styles.bedStatus}>
-                      <img src={bed.svg || checkIcon} alt="icon-check" />
-                      <p className={styles.textBed}>
-                        {bed.isStock ? "Не в наявності" : "В наявності"}
-                      </p>
-                    </div>
-                    <p className={styles.textPrice}>{bed.price}</p>
-                  </div>
-                  <button
-                    className={styles.btnDelete}
-                    onClick={() => handleDeleteItem(index)}
-                  >
-                    <img src={deleteSvg} alt="deleteSvg" />
-                  </button>
-                </li>
-                <hr className={styles.dash}  />
-              </>
-              );
-            })}
-            <div className={styles.shadowContainer}>
-              {!showAll && <div className={styles.shadowFade}></div>}
-              <button
-                onClick={toggleShowAll}
-                className={styles.buttonShowAll}
-              >
-                {showAll ? "Сховати" : "Показати всі"}
-                <img
-                  src={arrowShow}
-                  alt="arrow-show"
-                  className={`${styles.arrowIcon} ${showAll ? styles.rotated : ""
-                    }`}
-                />
-              </button>
-            </div>
-          </ul>
-          <div className={styles.containerPromocode}>
-            <div className={styles.promocodeDecription}>
-              <img src={promoIcon} alt="promo-icon" />
-              <p>Є промокод?</p>
-            </div>
-            <div className={styles.promocodeDecription}>
-              <input
-                type="text"
-                placeholder="Введіть код"
-                className={styles.inputPromocode}
-                value={promoInputValue}
-                disabled={items.length === 0}
-                onChange={(e) => setPromoInputValue(e.target.value)}
-              />
-              <button
-                className={styles.buttonPromocode}
-                onClick={applyPromo}
-              >
-                Ок
-              </button>
-            </div>
-          </div>
-          <p id="message">{promoMessage}</p>
-          <hr className={styles.dash}/>
-          <div style={{marginLeft:20}}>
-            <p className={styles.textTogether} style={{marginBottom:28}}>Разом:</p>
-            <ul className={styles.listContainer}>
-              <li className={styles.togetherSum}>
-                <p className={styles.textSum}>
-                  {counts.reduce((acc, count) => acc + count, 0)}{" "}
-                  товари на суму:
-                </p>
-                <div className={styles.lineDotted}></div>
-                <p>{getTotalPrice().toLocaleString("uk-UA")} грн</p>
-              </li>
-              <li className={styles.togetherSum}>
-                <p className={styles.textSum}> Вартість доставки:</p>
-                <div className={styles.lineDotted}></div>
-                <p>За тарифами оператора</p>
-              </li>
-              <li className={styles.togetherSum}>
-                <p className={styles.textSum}>До оплати:</p>
-                <div className={styles.lineDotted}></div>
-                <p className={styles.textTogether}>
-                  {getTotalPrice().toLocaleString("uk-UA")} грн.
-                </p>
-              </li>
-            </ul>
-          </div>
-          <div className={styles.containerBed}>
-              <input type="checkbox" className={styles.checkboxes}/>
-              <p style={{textAlign:"left"}}>Не передзвонюйте мені для підтвердження замовлення</p>
-            </div>
-          <button
-            className={styles.btnOrder}
-            onClick={orderButton}
-          >
-            ОФОРМИТИ ЗАМОВЛЕННЯ
-          </button>
-          <p>
-            Підтверджуючи замовлення, я приймаю умови{" "}
-            <span className={styles.userOkay}>
-              <Link to="/agree" style={{color:"#FFBC57"}}>Угоди користувача</Link>
-              </span>
-          </p>
-        </div>
-      </section>
-    </Fragment>
-  );
-
-};
-
-
-
-
-
-
-
-
-
+		return total
+	}
+	const { orderButton } = useContext(ContextBasketList)
+	return (
+		<Fragment>
+			<section>
+				<div className={styles.wrapperBasket}>
+					<p className={styles.textBasket}>
+						Ваш кошик
+						<span className={styles.textBasketNumber}>
+							{counts.reduce((acc, count) => acc + count, 0)}
+						</span>
+					</p>
+					<ul>
+						{items.map((bed, index) => {
+							const isHidden = !showAll && index >= 2
+							return (
+								<>
+									<li
+										key={index}
+										className={`${styles.containerBed} ${
+											isHidden ? styles.hiddenOnMobile : ''
+										}`}
+									>
+										<div className={styles.containerDescription}>
+											<img
+												src={bed.image.src}
+												alt={bed.alt}
+												className={styles.imgBed}
+											/>
+											<div className={styles.containerCount}>
+												<button
+													onClick={() => handleMinusItem(index)}
+													className={styles.btnMath}
+												>
+													-
+												</button>
+												<p>{counts[index]}</p>
+												<button
+													onClick={() => handleAddItem(index)}
+													className={styles.btnMath}
+												>
+													+
+												</button>
+											</div>
+										</div>
+										<div className={styles.containerDescription}>
+											<p className={styles.textBed}>{bed.size}</p>
+											<p className={styles.textDescription}>
+												{bed.description}
+											</p>
+											<div className={styles.bedStatus}>
+												<img src={bed.svg || checkIcon} alt='icon-check' />
+												<p className={styles.textBed}>
+													{bed.isStock ? 'Не в наявності' : 'В наявності'}
+												</p>
+											</div>
+											<p className={styles.textPrice}>{bed.price}</p>
+										</div>
+										<button
+											className={styles.btnDelete}
+											onClick={() => handleDeleteItem(index)}
+										>
+											<img src={deleteSvg} alt='deleteSvg' />
+										</button>
+									</li>
+									<hr className={styles.dash} />
+								</>
+							)
+						})}
+						<div className={styles.shadowContainer}>
+							{!showAll && <div className={styles.shadowFade}></div>}
+							<button onClick={toggleShowAll} className={styles.buttonShowAll}>
+								{showAll ? 'Сховати' : 'Показати всі'}
+								<img
+									src={arrowShow}
+									alt='arrow-show'
+									className={`${styles.arrowIcon} ${
+										showAll ? styles.rotated : ''
+									}`}
+								/>
+							</button>
+						</div>
+					</ul>
+					<div className={styles.containerPromocode}>
+						<div className={styles.promocodeDecription}>
+							<img src={promoIcon} alt='promo-icon' />
+							<p>Є промокод?</p>
+						</div>
+						<div className={styles.promocodeDecription}>
+							<input
+								type='text'
+								placeholder='Введіть код'
+								className={styles.inputPromocode}
+								value={promoInputValue}
+								disabled={items.length === 0}
+								onChange={(e) => setPromoInputValue(e.target.value)}
+							/>
+							<button className={styles.buttonPromocode} onClick={applyPromo}>
+								Ок
+							</button>
+						</div>
+					</div>
+					<p id='message'>{promoMessage}</p>
+					<hr className={styles.dash} />
+					<div style={{ marginLeft: 20 }}>
+						<p className={styles.textTogether} style={{ marginBottom: 28 }}>
+							Разом:
+						</p>
+						<ul className={styles.listContainer}>
+							<li className={styles.togetherSum}>
+								<p className={styles.textSum}>
+									{counts.reduce((acc, count) => acc + count, 0)} товари на
+									суму:
+								</p>
+								<div className={styles.lineDotted}></div>
+								<p>{getTotalPrice().toLocaleString('uk-UA')} грн</p>
+							</li>
+							<li className={styles.togetherSum}>
+								<p className={styles.textSum}> Вартість доставки:</p>
+								<div className={styles.lineDotted}></div>
+								<p>За тарифами оператора</p>
+							</li>
+							<li className={styles.togetherSum}>
+								<p className={styles.textSum}>До оплати:</p>
+								<div className={styles.lineDotted}></div>
+								<p className={styles.textTogether}>
+									{getTotalPrice().toLocaleString('uk-UA')} грн.
+								</p>
+							</li>
+						</ul>
+					</div>
+					<div className={styles.containerBed}>
+						<input type='checkbox' className={styles.checkboxes} />
+						<p style={{ textAlign: 'left' }}>
+							Не передзвонюйте мені для підтвердження замовлення
+						</p>
+					</div>
+					<button className={styles.btnOrder} onClick={orderButton}>
+						ОФОРМИТИ ЗАМОВЛЕННЯ
+					</button>
+					<p>
+						Підтверджуючи замовлення, я приймаю умови{' '}
+						<span className={styles.userOkay}>
+							<Link to='/agree' style={{ color: '#FFBC57' }}>
+								Угоди користувача
+							</Link>
+						</span>
+					</p>
+				</div>
+			</section>
+		</Fragment>
+	)
+}

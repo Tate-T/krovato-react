003201c51bb99610fa2c014c080ad46216c0e5b1
ByteMe6 <tsapiy.yurka@gmail.com>
2025-11-01 19:01:02 +0200

    hotfix

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { removeFavorite } from "../features/favorite/favoriteSlice";
-import styleFavorite from "./Favorite.module.css"; // твій CSS модуль
+// import React from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { removeFavorite } from "../features/favorite/favoriteSlice";
+// import styleFavorite from "./Favorite.module.css"; // твій CSS модуль
 
 const Favorite = ({ isOpen, onClose }) => {
-  const dispatch = useDispatch();
-  const products = useSelector(state => state.favorite.products);
+//   const dispatch = useDispatch();
+//   const products = useSelector(state => state.favorite.products);
 
-  if (!isOpen) return null;
+//   if (!isOpen) return null;
 
-  return (
-    <div className={styleFavorite.Favorite}>
-      <button className={styleFavorite.closeBtn} onClick={onClose}>
-        &#10005;
-      </button>
-      {products.length === 0 ? (
-        <p>немає вибраних товарів</p>
-      ) : (
-        products.map(item => (
-          <div key={item.id} className={styleFavorite.item}>
-            <span>{item.name}</span>
-            <button onClick={() => dispatch(removeFavorite(item.id))}>Видалити</button>
-          </div>
-        ))
-      )}
-    </div>
-  );
+//   return (
+//     <div className={styleFavorite.Favorite}>
+//       <button className={styleFavorite.closeBtn} onClick={onClose}>
+//         &#10005;
+//       </button>
+//       {products.length === 0 ? (
+//         <p>немає вибраних товарів</p>
+//       ) : (
+//         products.map(item => (
+//           <div key={item.id} className={styleFavorite.item}>
+//             <span>{item.name}</span>
+//             <button onClick={() => dispatch(removeFavorite(item.id))}>Видалити</button>
+//           </div>
+//         ))
+//       )}
+//     </div>
+//   );
 };
 
 export default Favorite;
